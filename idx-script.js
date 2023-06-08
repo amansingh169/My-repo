4c8b8c704e7b397aac1c4dbaedd73ccf3873e650
NerdAman <59738911+amansingh169@users.noreply.github.com>
2023-06-08 23:15:21 +0530

    Add Explore dropdown

--- idx-script.js
+++ idx-script.js
@@ -1,11 +1,51 @@
-$(document).on('scroll', function(){
-    if ( $(window).scrollTop() > 30) {
-        $('#header').addClass('change');
-    } else {
-        $('#header').removeClass('change');
-    }
-});
+
 
 function show(anything){
     document.querySelector('.options').value = anything;
 }
+
+function show(anything) {
+    document.querySelector('.dropdown').value = anything;
+}
+
+// let drop = document.querySelector('.dropdown');
+// drop.onclick = function(){
+//     drop.classList.toggle('active');
+// }
+
+function drop() {
+    var droping = document.getElementById("options");
+    droping.classList.toggle("active");
+}
+
+// let droping = document.querySelector('.options');
+// droping.onclick = function(){
+//     droping.classList.toggle('active');
+// }
+
+// function loadmore() {
+//     var ext = document.getElementsById("gallery");
+//     ext.classList.toggle("lm");
+// }
+
+$(document).ready(function () {
+    
+    //jQuery code here
+    
+    $(document).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.header').addClass('change');
+        } else {
+            $('.header').removeClass('change');
+        }
+    });
+
+    $('.top').click(function () {
+        // console.log('You clicked on top', this);
+        $(this).toggleClass('change1');
+    });
+
+    $(".top").hover(function(){
+
+    });
+});
